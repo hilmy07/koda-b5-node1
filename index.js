ea2a453df5650de05e2936decdf062ce7c2a7ca1
unknown <haidaraly98@gmail.com>
2025-11-11 13:09:43 +0700

    feat: add init project

---
+++ index.js
@@ -0,0 +1,25 @@
+// konversi tanggal ex. 24-04-2015 to 24/04/2015 menggunakan moment
+
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+const moment = require('moment');
+
+function convertDateFormat(dateString) {
+    const date = moment(dateString, 'DD-MM-YYYY');
+
+    return date.format('DD/MM/YYYY');
+}
+
+// Contoh penggunaan
+// const inputDate = '24-04-2015';
+// const outputDate = convertDateFormat(inputDate);
+// console.log(`Converted date: ${outputDate}`); 
+
+rl.question('Masukkan tanggal (DD-MM-YYYY): ', (inputDate) => {
+    const outputDate = convertDateFormat(inputDate);
+    console.log(`Tanggal yang dikonversi: ${outputDate}`);
+    rl.close();
+});
