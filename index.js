fd676d01cd0f7f9baf9c00dc0d5898b91fd4459f
unknown <haidaraly98@gmail.com>
2025-11-11 21:31:15 +0700

    fix: update project moment dan tanpa moment

--- index.js
+++ index.js
@@ -7,27 +7,59 @@ const rl = readline.createInterface({ input, output });
 
 const moment = require("moment");
 
-function convertDateFormat(dateString) {
-    const date = moment(dateString, "DD-MM-YYYY");
-
-    return date.format("DD/MM/YYYY");
-}
-
 // Contoh penggunaan
 // const inputDate = '24-04-2015';
 // const outputDate = convertDateFormat(inputDate);
 // console.log(`Converted date: ${outputDate}`); 
 
-rl.question("Masukkan tanggal (DD-MM-YYYY): ", (inputDate) => {
-    const datePattern = /^\d{2}-\d{2}-\d{4}$/;
+function kesatu() {
+    function convertDateFormat(dateString) {
+        const date = moment(dateString, "DD-MM-YYYY");
 
-    if (!datePattern.test(inputDate)) {
-        console.log("not valid format!");
-        rl.close();
-        return;
+        return date.format("DD/MM/YYYY");
     }
 
-    const outputDate = convertDateFormat(inputDate);
-    console.log(`Tanggal yang dikonversi: ${outputDate}`);
-    rl.close();
+    rl.question("Masukkan tanggal (DD-MM-YYYY): ", (inputDate) => {
+        const outputDate = convertDateFormat(inputDate);
+        console.log(`Tanggal yang dikonversi: ${outputDate}`);
+        rl.close();
+    });
+}
+
+function kedua() {
+    function formatDate(dateString) {
+        return dateString.replace(/-/g, "/");
+    }
+
+    rl.question("Masukkan tanggal (DD-MM-YYYY): ", (inputDate) => {
+        // regex validasi format
+        const datePattern = /^\d{2}-\d{2}-\d{4}$/;
+
+        if (!datePattern.test(inputDate)) {
+            console.log("not valid format!");
+            rl.close();
+            return;
+        }
+
+        const outputDate = formatDate(inputDate);
+        console.log(`Tanggal yang dikonversi: ${outputDate}`);
+        rl.close();
+    });
+}
+
+console.log("=== pilih konverter Date ===");
+console.log("1. Menggunakan moment");
+console.log("2. Tanpa menggunakan moment");
+
+rl.question("Masukkan pilihan: ", (pilihan) => {
+
+    if(pilihan==1) {
+        kesatu();
+        // rl.close();
+    } 
+    
+    if (pilihan==2) {
+        kedua();
+        // rl.close();
+    }
 });
